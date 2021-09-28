7dbd72098b82d69e5aec5b1142a1931d4a69259e
Lucas Bortoli <lucasbortolidev@gmail.com>
2021-09-27 21:37:16 -0300

    fix: resolve errors in example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
-import JitsiMeet from 'react-native-jitsimeet';
+import JitsiMeet from '@bortolilucas/react-native-jitsimeet';
 
 export default function App() {
   const onPress = () => {
